2acd4754b3d917e7cbcab41741f7af4f98621db8
Darcy Glennen <darcy@portable.com.au>
2020-09-29 21:28:14 +1000

    Implement basic UI component to add further library usage examples

---
+++ template/next-ts-chakra-ui/src/components/ui/Card/Card.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Heading, Link, Stack, Text } from '@chakra-ui/core';
+import type { StackProps } from '@chakra-ui/core';
+
+import { rem } from 'polished';
+
+type Props = {
+  title: string;
+  description?: string;
+  link?: {
+    label: string;
+    url: string;
+  };
+} & StackProps;
+
+const Card: React.FC<Props> = ({ title, description, link, ...rest }) => {
+  return (
+    <Stack
+      backgroundColor="teal.100"
+      borderRadius="md"
+      color="gray.900"
+      padding={{ base: 4, lg: 8 }}
+      spacing={{ base: 2, lg: 4 }}
+      {...rest}
+    >
+      <Heading fontSize={{ base: 'xl', lg: '2xl' }}>{title}</Heading>
+      {description && (
+        <Text fontSize={{ base: 'lg', lg: 'xl' }}>{description}</Text>
+      )}
+      {link && (
+        <Link href={link.url} fontSize={rem('15px')} textDecoration="underline">
+          {link.label}
+        </Link>
+      )}
+    </Stack>
+  );
+};
+
+export default Card;
