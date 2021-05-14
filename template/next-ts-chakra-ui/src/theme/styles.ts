3590db1b196f7a66f1ed701710b3765daf55f9ec
Darcy Glennen <darcy@portable.com.au>
2021-05-14 17:21:15 +1000

    Simplify Chakra UI global styles

--- template/next-ts-chakra-ui/src/theme/styles.ts
+++ template/next-ts-chakra-ui/src/theme/styles.ts
@@ -4,13 +4,8 @@ import type { Styles } from '@chakra-ui/theme-tools';
 // https://chakra-ui.com/docs/features/global-styles
 const styles: Styles = {
   global: {
-    html: {
-      height: '100%',
-    },
-    body: {
-      height: '100%',
-    },
-    '#root, #__next': {
+    // Make our page containers fill the height of the browser
+    'html, body, #root, #__next': {
       height: '100%',
     },
   },
