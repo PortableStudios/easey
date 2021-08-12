f337c7f451b2a2f54ec24b130794499b4e36d034
Darcy Glennen <darcy@portable.com.au>
2021-08-12 18:24:25 +1000

    Add a "displays 404 message on load" Cypress test to utilise `cypress-testing-library` method

--- template/next-ts-chakra-ui/cypress/integration/404.spec.ts
+++ template/next-ts-chakra-ui/cypress/integration/404.spec.ts
@@ -5,6 +5,11 @@ describe('404', () => {
     cy.visit('/foobar', { failOnStatusCode: false });
   });
 
+  it('displays 404 message on load', () => {
+    cy.visit('/foobar', { failOnStatusCode: false });
+    cy.findByText(/not found/i).should('be.visible');
+  });
+
   it('returns 404 status code', () => {
     cy.request({ url: '/foobar', failOnStatusCode: false }).then((response) => {
       expect(response.status).to.equal(404);
