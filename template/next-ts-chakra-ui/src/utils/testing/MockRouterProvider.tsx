38eca7eda3f5df31f9187d49be35951d7686a613
Darcy Glennen <darcy@portable.com.au>
2021-08-12 18:02:14 +1000

    Fix type error in `MockRouterProvider.ts` caused by Next.js upgrade

--- template/next-ts-chakra-ui/src/utils/testing/MockRouterProvider.tsx
+++ template/next-ts-chakra-ui/src/utils/testing/MockRouterProvider.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RouterContext } from 'next/dist/next-server/lib/router-context';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
 import type { NextRouter } from 'next/router';
 
 // https://github.com/vercel/next.js/issues/7479#issuecomment-659859682
