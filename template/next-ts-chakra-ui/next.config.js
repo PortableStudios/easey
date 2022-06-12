5780db3819ad665a5997d4ca618cd4db2c59789d
Darcy Glennen <darcyglennen@gmail.com>
2022-06-12 22:04:39 +1000

    fix: update `next` config to use `withSentryConfig` directly to fix `webpack` config not applying

--- template/next-ts-chakra-ui/next.config.js
+++ template/next-ts-chakra-ui/next.config.js
@@ -7,30 +7,32 @@ const ANALYZE = process.env.ANALYZE || '';
 const SENTRY_ENVIRONMENT = process.env.SENTRY_ENVIRONMENT || '';
 
 module.exports = withPlugins(
-  [
-    withSentryConfig({}, { dryRun: SENTRY_ENVIRONMENT === '' }),
-    bundleAnalyzerPlugin({ enabled: ANALYZE === 'true' }),
-  ],
-  {
-    webpack5: true,
-    swcMinify: true,
-    poweredByHeader: false,
-    reactStrictMode: true,
-    webpack: (config) => {
-      // Insert our custom polyfill file in to the beginning of the main entry point
-      // https://github.com/vercel/next.js/issues/13231#issuecomment-711484680
-      const originalEntry = config.entry;
-      config.entry = async () => {
-        const entries = await originalEntry();
-        const main = entries['main.js'];
-        if (main && !main.includes('./src/polyfills.ts')) {
-          main.unshift('./src/polyfills.ts');
-        }
+  [bundleAnalyzerPlugin({ enabled: ANALYZE === 'true' })],
+  // Use `withSentryConfig` directly as it's not compatible with `next-compose-plugins`
+  // https://github.com/getsentry/sentry-javascript/issues/3579#issuecomment-856325385
+  withSentryConfig(
+    {
+      webpack5: true,
+      swcMinify: true,
+      poweredByHeader: false,
+      reactStrictMode: true,
+      webpack: (config) => {
+        // Insert our custom polyfill file in to the beginning of the main entry point
+        // https://github.com/vercel/next.js/issues/13231#issuecomment-711484680
+        const originalEntry = config.entry;
+        config.entry = async () => {
+          const entries = await originalEntry();
+          const main = entries['main.js'];
+          if (main && !main.includes('./src/polyfills.ts')) {
+            main.unshift('./src/polyfills.ts');
+          }
 
-        return entries;
-      };
+          return entries;
+        };
 
-      return config;
+        return config;
+      },
     },
-  }
+    { dryRun: SENTRY_ENVIRONMENT === '' }
+  )
 );
