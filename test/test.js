771ee33e33ae99eb5d3ecc7370e896d81a408497
Darcy Glennen <darcyglennen@gmail.com>
2022-06-12 22:59:56 +1000

    chore: remove snapshot testing

--- test/test.js
+++ test/test.js
@@ -1,25 +1,21 @@
-const path = require('path');
-const test = require('ava');
-const sao = require('sao');
+const path = require("path");
+const test = require("ava");
+const sao = require("sao");
 
-const generator = path.join(__dirname, '..');
+const generator = path.join(__dirname, "..");
 
 // Generate a project using the "Next.js" option
-test('Next.js template', async (t) => {
+test("Next.js template", async (t) => {
   const answers = {
-    name: 'portable-project',
-    description: 'A thing that does stuff',
-    author: 'John Smith <john@portable.com.au>',
-    type: 'nextjs',
+    name: "portable-project",
+    description: "A thing that does stuff",
+    author: "John Smith <john@portable.com.au>",
+    type: "nextjs",
   };
   const stream = await sao.mock({ generator }, answers);
 
-  // Create a snapshot of the directory contents (ignoring .DS_Store files for cross-platform compatibility)
-  const files = stream.fileList.filter((path) => !path.includes('.DS_Store'));
-  t.snapshot(files, 'Generated files');
-
   // Read the generated package.json and check the information is correct
-  const pkg = JSON.parse(await stream.readFile('package.json'));
+  const pkg = JSON.parse(await stream.readFile("package.json"));
   t.is(pkg.name, answers.name);
   t.is(pkg.description, answers.description);
   t.is(pkg.author, answers.author);
