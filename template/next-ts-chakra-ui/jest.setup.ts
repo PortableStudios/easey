7df44197f118a8cc55cf9ae866821ffc60899a03
Darcy Glennen <darcyglennen@gmail.com>
2022-06-12 23:05:46 +1000

    feat: integrate `@storybook/testing-react` to import stories in tests
    https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests

--- template/next-ts-chakra-ui/jest.setup.ts
+++ template/next-ts-chakra-ui/jest.setup.ts
@@ -1,5 +1,10 @@
 import { cleanup } from '@testing-library/react';
+import { setGlobalConfig } from '@storybook/testing-react';
 import '@testing-library/jest-dom/extend-expect';
+
+import * as globalStorybookConfig from './.storybook/preview';
+
+setGlobalConfig(globalStorybookConfig);
 
 afterEach(() => {
   cleanup();
